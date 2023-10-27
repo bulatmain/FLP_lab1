f74e3d72aeb8db1f89d8365bbe2c67ff920841ef
bulat <sinntexxx@gmail.com>
2023-10-28 02:36:40 +0300

    Task 1 finished

--- task1.pl
+++ task1.pl
@@ -1,7 +1,24 @@
-% ѕерва€ часть задани€ - предикаты работы со списками
-
-% Ќиже - пример кода, который вы можете удалить.
-
+% Length of list
 mylen([],0).
 mylen([X|T],N) :- mylen(T,N1), N is N1+1.
 
+% Is X member of list
+mymember(X, [X|_]).
+mymember(X, [_|T]) :- mymember(X, T).
+
+% Out argument is two appended lists
+myappend([], L, L).
+myappend([X|T], L, [X|R]) :- myappend(T, L, R).  
+
+% Out argument is list without one entry of given element
+myremove([X|T], X, T).
+myremove([H|T], X, [H|R]) :- myremove(H, X, R).
+
+% Two lists are permutations of each other
+mypermute([], []).
+mypermute(L, [X|T]) :-
+    myremove(L, X, R),
+    mypermute(L, R).
+
+% R is sublist of L
+mysublist(R, L) :- myappend(_, T, L), myappend(R, _, T).
