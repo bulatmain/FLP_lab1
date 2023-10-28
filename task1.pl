628bae59a25fdcb7e8e97c15a7e001d9320db26f
bulat <sinntexxx@gmail.com>
2023-10-28 03:46:25 +0300

    Task1 totally finished

--- task1.pl
+++ task1.pl
@@ -1,3 +1,5 @@
+% Point 3
+
 % Length of list
 mylen([],0).
 mylen([X|T],N) :- mylen(T,N1), N is N1+1.
@@ -22,3 +24,38 @@ mypermute(L, [X|T]) :-
 
 % R is sublist of L
 mysublist(R, L) :- myappend(_, T, L), myappend(R, _, T).
+
+% Point 4
+
+% First option
+remove_last_three(R, L) :- 
+    myappend(L, T, R),
+    mylen(T, N),
+    N = 3.
+
+% Second option
+len_is_three(L) :-
+    mylen(L, N),
+    N = 3.
+
+just_is(A, A). 
+
+rm_last_three(R, []) :-
+    len_is_three(R), !.
+rm_last_three([H|T], [H]) :-
+    len_is_three(T), !.
+rm_last_three([H|T], L) :-
+    rm_last_three(T, L1),
+    just_is(L, [H|L1]).
+
+% Point 5
+
+sum([], 0).
+sum([H|T], S) :-
+    sum(T, S1),
+    S is H + S1.
+
+average(L, A) :-
+    sum(L, S),
+    mylen(L, N),
+    A is S / N.
